97daf67766d77aed3370c0111ad6d3375f7eb74f
jrbrad <jamesrbradley@cox.net>
2024-01-22 18:33:21 -0500

    Update useful_functions.py

--- Lecture/useful_functions.py
+++ Lecture/useful_functions.py
@@ -5,12 +5,12 @@ Created on Thu Mar 23 07:49:59 2023
 @author: jrbrad
 """
 
-def cos_sim(vec1, vec2):
-    d_prod = sum(vec1[i]*vec2[i] for i in range(len(vec1)))
-    return d_prod/(dist((0,0), vec1) * dist((0,0), vec2))
+from numpy import linalg
+import numpy as np
 
-def dist(loc1, loc2):
-    return ((loc1[0] - loc2[0])**2 + (loc1[1] - loc2[1])**2)**0.5
+def cos_sim(vec1, vec2):
+    vec1,vec2 = np.array(vec1), np.array(vec2)
+    return vec1@vec2/(linalg.norm(vec1) * linalg.norm(vec2))
 
 if __name__ == '__main__':
     print(dist((0,0), (3,4)))
