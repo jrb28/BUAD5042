57271bb3f7d848e689df42b8e70fc3006a77ffa8
jrb28 <james.bradley@mason.wm.edu>
2024-02-23 07:50:35 -0500

    Add files via upload

---
+++ Multiprocessing/import_test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 23 07:44:45 2024
+
+@author: jrbrad
+"""
+
+def add_up(x,y):
+    return x+y
+
+print(f'Hello, my name is {__name__} and I am starting!')
+print(f'{add_up(1,2)}')
---
+++ Multiprocessing/test_import_test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 23 07:47:11 2024
+
+@author: jrbrad
+"""
+
+import import_test
+
+print(f'I just started import_test and my name is {__name__}.')
+y = import_test.add_up(3,4)
+print(y)
